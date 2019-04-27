efb0363b9f1dba2db740ab59af7363fd91e6b36f
wqzwh <wangziqihappy@163.com>
2019-04-27 14:16:33 +0800

    feature:日志相关功能添加

--- server-1/app.js
+++ server-1/app.js
@@ -2,6 +2,7 @@ const querystring = require('querystring')
 const handlerBlogRouter = require('./src/router/blog')
 const handlerUserRouter = require('./src/router/user')
 const { get, set } = require('./src/db/redis')
+const { access } = require('./src/utils/log')
 
 // session 数据
 // const SESSION_DATA = {}
@@ -41,6 +42,13 @@ const getPostData = req => {
 }
 
 const serverHandle = (req, res) => {
+  // 记录accesslog
+  access(
+    `${req.method} -- ${req.url} -- ${
+      req.headers['user-agent']
+    } -- ${Date.now()}`
+  )
+
   // 设置返回格式为json
   res.setHeader('Content-type', 'application/json')
 
